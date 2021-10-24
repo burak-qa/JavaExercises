d6f7d3114a85a3fb0d542225a483cfa5b4e8f255
Burak <sarisahinburak22@gmail.com>
2021-10-24 13:54:08 +0200

    initialcommit

---
+++ ExercisesDay1.java
@@ -0,0 +1,10 @@
+public class ExercisesDay1 {
+    //Main Method
+    public static void main(String[] args) {
+        average(85.2, 90.3);  //method call
+    };
+    //Method
+    public static void average(double num1, double num2) {
+    System.out.println("the average of the two numbers you entered:" + (num1+num2)/2);
+    };
+}
