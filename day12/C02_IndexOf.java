9ce796581f7756b49a693ba889b5e891890ef8d8
Burak <sarisahinburak22@gmail.com>
2021-11-16 21:51:57 +0100

    mod

---
+++ day12/C02_IndexOf.java
@@ -0,0 +1,35 @@
+package day12;
+
+import java.util.Scanner;
+
+public class C02_IndexOf {
+    public static void main(String[] args) {
+        //Soru 2) Kullanicidan bir cumle ve bir kelime isteyin, 
+		//kelimenin cumledeki kullanimina bakarak asagidaki 3 cumleden uygun olani yazdirin
+		 // - Girilen kelime cumlede kullanilmamis.
+		 //- Girilen kelime cumlede 1 kere kullanilmis.
+		 // - Girilen kelime cumlede 1’den fazla kullanilmis.
+		
+		
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Lutfen bir cumle giriniz");
+		String cumle=scan.nextLine();
+		
+		System.out.println("Lutfen varligini kontrol etmek icin bir kelime giriniz");
+		String kelime=scan.next();
+		
+		int index=cumle.indexOf(kelime);
+		
+		if (index<0) {
+			System.out.println("Girilen kelime cumlede kullanilmamis");
+		} else {
+			if (cumle.indexOf(kelime, index+1)<0) { //(kelime, index+1)==(-1) seklinde de yazabiliriz
+				System.out.println("Girilen kelime bir kere kullanilmis");
+			} else {
+				System.out.println("Girilen kelime birden fazla kullanilmis");
+			}
+		}
+		scan.close();
+    }
+}
---
+++ day12/C06_Contains.java
@@ -0,0 +1,35 @@
+package day12;
+
+import java.util.Scanner;
+
+public class C06_Contains {
+    public static void main(String[] args) {
+        // Kullanicidan email adresini girmesini isteyin, mail @gmail.com
+		//icermiyorsa "lutfen gmail mail adresi giriniz" @gmail.com ile bitiyorsa “Email adresiniz kaydedildi”
+		// yazdirin
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Lutfen email adresinizi giriniz...     : ");
+		String email=scan.next();
+		
+		int uzunluk=email.length();
+		int index=email.lastIndexOf("@gmail.com");
+		
+		if (!email.contains("@gmail.com")) {
+			System.out.println("Lutfen gmail mail adresi giriniz");
+		} else{
+			if (index==uzunluk-10) {
+				System.out.println("Email adresiniz basari ile kayit edildi.");
+			} else {
+				System.out.println("lutfen yazimi kontrol ediniz");
+			}
+			
+		}
+		
+
+		
+		
+		
+		scan.close();
+    }
+}
