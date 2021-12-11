2100c14a21f4fb823a3e7dc8af9536458bfe1082
Burak <sarisahinburak22@gmail.com>
2021-12-11 19:40:26 +0100

    mod

---
+++ day31_dateAndTime/C04_DateTime.java
@@ -0,0 +1,22 @@
+package day31_dateAndTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C04_DateTime {
+
+	public static void main(String[] args) {
+		//bir String ve bir int variable olusturalim
+		//bir loop icerisinde bu variab'lari 1000 kere degistirelim 
+		//islem surelerini kiyaslayalim ?
+		
+		
+		// Verilen iki dogum tarihinin hangisinin daha once oldugunu bulunuz
+		
+		
+		String str="Burak";
+		List<Integer> list = new ArrayList<>();
+		str.toLowerCase();
+	}
+
+}
