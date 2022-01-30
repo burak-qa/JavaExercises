ff2df3c93c0f6851b77b3072859820a2244b3d50
Burak <sarisahinburak22@gmail.com>
2022-01-30 21:40:47 +0100

    mod

---
+++ com.Batch44JUnit/src/test/java/day12/C05_KeyboardActions.java
@@ -0,0 +1,41 @@
+package day12;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class C05_KeyboardActions  extends TestBase {
+    //1- Bir Class olusturalim KeyboardActions1
+    //2- https://www.amazon.com sayfasina gidelim
+    //3- Arama kutusuna actions method’larine kullanarak samsung A71 yazdirin ve Enter’a basarak arama yaptirin
+    //4- aramanin gerceklestigini test edin
+
+    @Test
+    public void test01() throws InterruptedException {
+        driver.get("https://www.amazon.com");
+
+        WebElement searchBox=driver.findElement(By.id("twotabsearchtextbox"));
+
+        Actions actions=new Actions(driver);
+        actions.click(searchBox).
+                keyDown(Keys.SHIFT).
+                sendKeys("s").
+                keyUp(Keys.SHIFT).
+                sendKeys("amsung ").
+                keyDown(Keys.SHIFT).
+                sendKeys("a").
+                keyUp(Keys.SHIFT).
+                sendKeys("71").
+                sendKeys(Keys.ENTER).
+                perform();
+
+
+        Thread.sleep(5000);
+
+
+
+    }
+}
