821d884e267e8e38830f0b452da67f624d88e549
Burak <sarisahinburak22@gmail.com>
2022-02-12 20:36:13 +0100

    mod

---
+++ com.CucumberBatch44/src/test/java/stepdefinitions/HerokuappStepDefinations.java
@@ -0,0 +1,31 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.HerokuappPage;
+import utilities.ReusableMethods;
+
+public class HerokuappStepDefinations {
+    HerokuappPage herokuappPage=new HerokuappPage();
+    @When("Add Element butonuna basar")
+    public void add_element_butonuna_basar() {
+        herokuappPage.addButonElement.click();
+    }
+    @Then("Delete butonu gorununceye kadar bekler")
+    public void delete_butonu_gorununceye_kadar_bekler() {
+        ReusableMethods.waitForVisibility(herokuappPage.deleteButonElement, 20);
+
+    }
+    @Then("Delete butonunun gorundugunu test eder")
+    public void delete_butonunun_gorundugunu_test_eder() {
+        Assert.assertTrue(herokuappPage.deleteButonElement.isDisplayed());
+
+    }
+    @Then("Delete butonuna basarak butonu siler")
+    public void delete_butonuna_basarak_butonu_siler() {
+        herokuappPage.deleteButonElement.click();
+    }
+
+}
